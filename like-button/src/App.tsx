d2e741a0fac6c83ed88ddc827f88b38bfe951efc
taqro <takurousaitou94@yahoo.co.jp>
2022-01-23 15:34:01 +0900

    ボタンの作成

---
+++ like-button/src/App.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <header className= "App-header">
+          <LikeButton />
+        </header>
+      </header>
+    </div>
+  );
+}
+
+function LikeButton(){
+  const [count, setCount] = useState(999);
+  const handleClick = () => {
+    setCount( count + 1);
+  }
+  return (
+  <span className="likeButton" onClick={handleClick}>
+    ♥ {count}
+  </span>
+  );
+}
+
+export default App;
